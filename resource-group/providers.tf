a50f44760d3600d107eb609fe16cd57bc0bd9a17
sid24desai <104271822+sid24desai@users.noreply.github.com>
2024-04-15 14:43:51 +0530

    Update providers.tf

--- resource-group/providers.tf
+++ resource-group/providers.tf
@@ -3,8 +3,8 @@
 provider "azurerm" {
   features {}
 
-  subscription_id = "0151c365-f598-44d6-b4fd-e2b6e97cb2a7"
-  client_id       = "c0c952e9-5254-45b5-b838-6d26a31435cb"
-  client_secret   = "Xvz8Q~kzNZdUWvMvOG_loZoz7OM5J7OTAdO-BaC3"
-  tenant_id       = "83331f4e-7f45-4ce4-99ed-af9038592395"
+  subscription_id = "xxxxxxxx"
+  client_id       = "xxxxxxx"
+  client_secret   = "xxxxxxxx"
+  tenant_id       = "xxxxxx"
 }
