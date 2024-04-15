ac9e9533d0e60876fc91a1b8dd12d41d1cad6a4c
sid24desai <104271822+sid24desai@users.noreply.github.com>
2024-04-15 13:31:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # Include resource group module
 module "rg_module" {
-  source              = "./terraform-modules/resource-group"  # Local path to your module directory
+  source              = "./resource-group"  # Local path to your module directory
   resource_group_name = var.resource_group_name  # Pass required variables to the module
   location            = var.location
 }
